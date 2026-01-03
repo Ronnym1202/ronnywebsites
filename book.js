035a517a01b6df05cb036766d7843cc9405ef588
@Ronnym1202 <ronnymwenda@gmail.com>
2026-01-03 18:58:49 +0300

    Adjusting 8

--- book.js
+++ book.js
@@ -3,7 +3,7 @@ const submit=document.getElementById("submit");
 const display=document.getElementById("display");
 form.addEventListener("submit",function(event){
     event.preventDefault();
-    display.textContent='YOUR BOOKING IS SUCCESSFUL, YOU WILL RECEIVE A CONFIRMATION MESSAGE';
+    display.textContent='BOOKING STATUS: YOUR BOOKING IS SUCCESSFUL, YOU WILL RECEIVE A CONFIRMATION MESSAGE';
 });
 
     
